728445e27fd23d5c9a98cec5bccee447ff6931b9
xDestyn <omar.flores.cs@outlook.com>
2021-11-28 13:37:27 -0600

    Create LessonService, LessonModule, and implement createLesson method

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
