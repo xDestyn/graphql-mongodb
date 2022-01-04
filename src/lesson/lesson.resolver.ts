4a92ac94b0daf87f0b22aaed18ab35bcaba17a62
xDestyn <omar.flores.cs@outlook.com>
2022-01-03 23:33:22 -0600

    Implement Validation: Create Lesson Input

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,4 +1,5 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateLessonInput } from './lesson.input';
 import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
@@ -11,7 +12,7 @@ export class LessonResolver {
 		return this.lessonService.getLesson(id);
 	}
 	@Mutation((returns) => LessonType)
-	createLesson(@Args('name') name: string, @Args('startDate') startDate: string, @Args('endDate') endDate: string) {
-		return this.lessonService.createLesson(name, startDate, endDate);
+	createLesson(@Args('createLessonInput') createLessonInput: CreateLessonInput) {
+		return this.lessonService.createLesson(createLessonInput);
 	}
 }
