5e0b0996a1471143f6c57a69bf215a3e491c8d07
xDestyn <omar.flores.cs@outlook.com>
2022-01-05 00:23:54 -0600

    Implement Student Resolver, Type, Entity, Service, DTO, and Module

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuid } from 'uuid';
+import { Repository } from 'typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+	constructor(@InjectRepository(Student) private studentRepository: Repository<Student>) {}
+
+	async createStudent(createStudentInput: CreateStudentInput): Promise<Student> {
+		const { firstName, lastName } = createStudentInput;
+
+		const student = await this.studentRepository.create({
+			id: uuid(),
+			firstName,
+			lastName,
+		});
+
+		return this.studentRepository.save(student);
+	}
+}
